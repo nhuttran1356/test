b78541438b395d6f21519fbf182dbd6b32586667
Nhut <nhuttran1356@gmail.com>
2022-09-06 15:12:17 +0700

    First commit

---
+++ hello2.c
@@ -0,0 +1,12 @@
+// version:	date:
+// Author:
+// Description
+
+#include <stdio.h> // printf function
+
+int main(void){
+	printf("Hello World\n");
+
+
+ 	return 0; 
+} 
---
+++ template.c
@@ -0,0 +1,12 @@
+// version:	date:
+// Author:
+// Description
+
+#include <stdio.h> // printf function
+
+int main(void){
+
+
+
+ 	return 0; 
+} 
